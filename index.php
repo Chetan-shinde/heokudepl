16ae37cad5a85906e744b49b57f7303b37bb3b2b
Chetan-shinde <chetanshinde120@gmail.com>
2018-01-15 15:24:55 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap Tags Input with Autocomplete</title>
+	<link href="vendor/typeahead/typeahead.css"  rel="stylesheet" />
+	<link href="vendor/tagsinput/bootstrap-tagsinput.css" rel="stylesheet">
+
+</head>
+<body>
+	<h1>Bootstrap Tags Input with Autocomplete</h1>
+	<div class="bgcolor">
+	<input type="text" value="" id="tags-input" data-role="tagsinput" />
+	</div>
+<h2>Hello Heroku I am chetan</h2>
+</body>
+</html>
